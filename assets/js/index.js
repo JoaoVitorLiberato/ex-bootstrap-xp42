105d1f4ab5b6086dbcdb8fe2d1bad99a01ec63cf
João Vitor L. de Oliveira <liberato_98@outlook.com>
2022-06-06 21:27:10 -0300

    Exercicio para praticar bootstrap e o uso de libs em um projeto.

---
+++ assets/js/index.js
@@ -0,0 +1,20 @@
+const cadastrarProduto = document.querySelector('.container #btn-cadastrarP')
+
+cadastrarProduto.addEventListener('click', () => 
+Toastify({
+    text: "Produto Cadastrado Com sucesso!",
+    duration: 3000,
+    destination: "https://github.com/apvarun/toastify-js",
+    newWindow: true,
+    close: true,
+    gravity: "bottom", // `top` or `bottom`
+    position: "right", // `left`, `center` or `right`
+    stopOnFocus: true, // Prevents dismissing of toast on hover
+    style: {
+      background: "#3c3c3d",
+      color: "#fff"
+    },
+    onClick: function(){} // Callback after click
+  }).showToast());
+
+
